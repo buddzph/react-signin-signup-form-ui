ddf451a0a7a1ae2ba7c4fc86ac963915ffe566dd
Frederick de Guzman <frederickdeguzman@gmail.com>
2019-02-09 14:54:48 +0800

    simple react and react-router project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Error from './components/Error/Error';
+import Navigation from './components/Navigation/Navigation';
+
+// const NewRoute = () => {
+//   return (
+//     <div>
+//       <p>New Route</p>
+//     </div>
+//   );
+// }
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        {/* <Route path='/new' component={NewRoute} /> */}
+        <div>
+          <Navigation />
+          <Switch>
+            <Route path='/' component={Home} exact />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route component={Error} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
